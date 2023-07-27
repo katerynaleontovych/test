09fec7a3ff6bb81c6e10a2a197db58f6a125dd29
katerynaleontovych <kateryna.leontovych@Kateryna.Leontovych>
2023-07-27 15:56:01 +0300

    my second adding file

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import sys
 import subprocess
 
 log_file = sys.argv[1]
+#log_file = sys.argv[1]
 
 # Викликайте  основний скрипт тут і передає значення log_file як аргумент командного рядка
 subprocess.call(['python', 'gf_script.py', log_file])
