f39d278461de1fc1aa0d0f291f134aa0f2d08dac
katerynaleontovych <kateryna.leontovych@Kateryna.Leontovych>
2023-07-30 13:52:35 +0300

    my second adding file

---
+++ testcopy.py
@@ -0,0 +1,9 @@
+import sys
+import subprocess
+
+log_file = sys.argv[1]
+#log_file = sys.argv[1]
+
+# Викликайте  основний скрипт тут і передає значення log_file як аргумент командного рядка
+subprocess.call(['python', 'gf_script.py', log_file])
+print("hi new feature")
