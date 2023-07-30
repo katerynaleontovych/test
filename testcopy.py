7411cb54585fd38757a312e5306d7ef05cef49ad
katerynaleontovych <kateryna.leontovych@Kateryna.Leontovych>
2023-07-30 13:57:27 +0300

    print feauture

--- testcopy.py
+++ testcopy.py
@@ -6,4 +6,4 @@ log_file = sys.argv[1]
 
 # Викликайте  основний скрипт тут і передає значення log_file як аргумент командного рядка
 subprocess.call(['python', 'gf_script.py', log_file])
-print("hi new feature")
+print("hi new feature g")
